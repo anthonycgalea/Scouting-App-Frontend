01a8d9ca8c530e2931f520963b610c96d6f40e1c
Anthony Galea <44701741+anthonycgalea@users.noreply.github.com>
2025-09-25 18:15:58 -0400

    Simplify export header formatting

--- src/components/ExportHeader/ExportHeader.tsx
+++ src/components/ExportHeader/ExportHeader.tsx
@@ -4,8 +4,8 @@ import { DownloadAsButton } from './DownloadAsButton';
 export function ExportHeader() {
   return (
     <Group justify="center">
-        <h1>Data Manager</h1>
-        <DownloadAsButton />
+      <h1>Data Validation</h1>
+      <DownloadAsButton />
     </Group>
   );
 }
---
+++ src/pages/DataValidation.page.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@mantine/core';
+import { DataManager } from '@/components/DataManager/DataManager';
+
+export function DataValidationPage() {
+  return (
+    <Box p="md">
+      <DataManager />
+    </Box>
+  );
+}
